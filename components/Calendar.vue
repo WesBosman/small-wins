e394f40cbd43a41b80a3b4454fa65d70f5b98fc7
Wes Bosman <bosmanwn@vcu.edu>
2020-01-23 21:42:07 -0500

    first commit

---
+++ components/Calendar.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="calendar">
+        <h1 class="calendar-header">
+            <b-button variant="outline-primary" type="button" v-on:click="previousWeek">{{"<"}}</b-button>
+            {{ month + " " + year}}
+            <b-button variant="outline-primary" type="button" v-on:click="nextWeek">{{">"}}</b-button>
+        </h1>
+        <div class="calendar-view">
+            <div class="calendar-weekday" v-for="(day, index) in dayAbbreviations" :key="index">
+                <div class="calendar-banner"></div>
+                <div class="calendar-day-banner"></div>
+                <span class="calendar-day" v-bind:class="isToday(numberedDaysOfWeek[index])">
+                    {{ numberedDaysOfWeek[index] }}
+                </span>
+                <div class="calendar-weekday-header">
+                    {{ day }}
+                </div>
+                <div class="calendar-weekday-content">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    computed: {
+        /**
+         * Need to check less than one and greater than end of the month values
+         */
+        numberedDaysOfWeek: function(){
+            let startDay = this.dayNumber - this.dayOfWeek;
+
+            return Array.from(Array(7).keys()).map(x => {
+                // Handle underflow to previous month
+                if(startDay + x < 1){
+                    return startDay + x + this.lastDayOfPreviousMonth;
+                }
+                // Handle overflow to next month
+                else if(startDay + x > this.lastDayOfMonth){
+                    return x + startDay - this.lastDayOfMonth;
+                }
+
+                return x + startDay
+            });
+        },
+        dayAbbreviations: function(){
+            return this.daysOfTheWeek.map(day => day.slice(0, 3).toUpperCase());
+        },
+        dayString: function(){
+            return this.daysOfTheWeek[new Date().getDay()];
+        }
+    },
+    data() {
+        let startDate = new Date();
+
+        return {
+            daysOfTheWeek: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            month: startDate.toLocaleString('default', { month: 'long' }),
+            dayNumber: startDate.getDate(),
+            year: startDate.getFullYear(),
+            dayOfWeek: startDate.getDay(),
+            lastDayOfMonth: new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0).getDate(),
+            lastDayOfPreviousMonth: new Date(startDate.getFullYear(), startDate.getMonth(), 0).getDate()
+        };
+    },
+    methods: {
+        previousWeek: function(){
+            console.log("Clicked previous week");
+        },
+        nextWeek: function(){
+            console.log("Clicked next week");
+        },
+        isToday: function(day){
+            let now = new Date().getDate();
+
+            return day === now ? "today" : "";
+        }
+    }
+}
+</script>
+<style>
+.calendar-header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  color: gray;
+}
+.calendar-view {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  column-gap: 10px;
+  justify-content: space-around;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+.calendar-banner {
+  height: 0px;
+  position: absolute;
+  margin-left: 5px;
+  width: 40px;
+  border-left: 25px solid #007bff;
+  border-right: 25px solid #007bff;
+  border-top: 60px solid #007bff;
+  border-bottom: 20px solid transparent;
+}
+.calendar-day-banner {
+  height: 0px;
+  position: absolute;
+  margin-left: 10px;
+  width: 30px;
+  border-left: 20px solid white;
+  border-right: 20px solid white;
+  border-top: 52px solid white;
+  border-bottom: 17px solid transparent;
+}
+.calendar-day {
+  position: absolute;
+  margin-top: 10px;
+  margin-left: 15px;
+  width: 30px;
+  height: 30px;
+  color: white;
+  text-align: center;
+  padding-top: 3px;
+  border-radius: 50%;
+  background-color: #007bff;
+}
+.calendar-day.today {
+  background-color: #ff6a00;
+}
+.calendar-weekday {
+  border: 3px solid #007bff;
+  height: 250px;
+}
+.calendar-weekday-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  border-bottom: 2px dashed gray;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container-fluid">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Small Wins</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    
+    <my-calendar></my-calendar>
+  </div>
+</template>
+
+<script>
+import Calendar from '~/components/Calendar.vue';
+
+export default {
+  components: {
+    'my-calendar': Calendar
+  },
+  data() {
+    return {
+
+    };
+  }
+}
+</script>
+
+<style>
+.container-fluid {
+  padding-left: 0px;
+  padding-right: 0px;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
