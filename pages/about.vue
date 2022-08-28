94bda4df5b9156be4afd23890b887fce671f4466
Wes Bosman <bosmanwn@vcu.edu>
2022-08-28 12:29:13 -0400

    Updated the about page.

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <NavBar/>
     <div class="content">
-        <h1>About</h1>
+        <h1>Keeping Score</h1>
         <p>
             This app was created to help me keep a running total of what I spend my money on.
         </p>
@@ -14,13 +14,16 @@
         </p>
         <h2>Getting Started</h2>
         <ol>
-            <li>Click the floating action button on the home screen to create a category.</li>
+            <li>Click the floating action button on the home screen then click the plus button to create a category.</li>
             <li>Once your category is created click the name of the category. This will take you to the category screen.</li>
-            <li>Click the floating action button on the category screen to add items to it.</li>
+            <li>Click the floating action button on the category screen then click the plus button to add items to it.</li>
             <li>Fill in the name and price of the item and click submit to add it to the category.</li>
             <li>Navigate back to the home screen by clicking the link in the header bar or clicking the category name at the top of the category screen.</li>
             <li>You should see a running total for each category you create on the home screen.</li>
         </ol>
+        <h2>Settings</h2>
+        <p>I have always liked apps that let me change themes. I'm going to try to keep that in mind as I build this application. I want you to be able to adjust this site to your liking. The settings will be stored in your browser using local storage.</p>
+        <p>Some of the settings might not update immediately. Try picking the settings you want then navigating to the home page to see their affect.</p>
     </div>
     
     <Footer/>
