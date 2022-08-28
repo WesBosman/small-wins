ef6830cd288ba7fbb44fa5211e85579419dc56fc
Wes Bosman <bosmanwn@vcu.edu>
2022-08-28 12:28:56 -0400

    Added a slide animation to the floating action button so I can put more buttons in that spot.

--- components/FloatingButton.vue
+++ components/FloatingButton.vue
@@ -3,33 +3,35 @@
         <!-- <div class="floating-button">
             <b-icon-trash-fill/>
         </div> -->
-        <a class="floating-button"
+        <a class="floating-button link button-two"
+            :class="this.slideout ? 'slideout' : ''"
             :style="style"
             href="/settings">
             <b-icon-gear/>
         </a>
-        <div class="floating-button" 
+        <div class="floating-button button-one" 
+            :class="this.slideout ? 'slideout' : ''"
             :style="style"
             @click="() => this.$emit('add-category-clicked')">
             <b-icon-plus/>
         </div>
-        <!-- <div class="three-dots floating-button" 
+        <div class="three-dots floating-button" 
             :class="this.slideout ? 'slideout' : ''" 
             :style="style"
             @click="onStartSlideOut">
-            <b-icon-three-dots-vertical/>
-        </div> -->
+            <b-icon-three-dots/>
+        </div>
     </div>
 </template>
 <script>
-import {BIconThreeDotsVertical, BIconPlus, BIconTrash, BIconGear} from 'bootstrap-vue';
+import {BIconThreeDots, BIconPlus, BIconTrash, BIconGear} from 'bootstrap-vue';
 
 export default {
     components: {
         'b-icon-gear': BIconGear,
         'b-icon-plus': BIconPlus,
         'b-icon-trash-fill': BIconTrash,
-        'b-icon-three-dots-vertical': BIconThreeDotsVertical
+        'b-icon-three-dots': BIconThreeDots
     },
     computed: {
         style: function() {
@@ -51,43 +53,57 @@ export default {
 }
 </script>
 <style>
-@keyframes rotate {
+@keyframes button-one-slide-up {
+    0% {
+        bottom: 0.5em;
+    }
     100% {
-        transform: rotate(90deg);
+        bottom: 2em;
     }
 }
-@keyframes slide-out {
-    from {
-
+@keyframes button-two-slide-up {
+    0% {
+        bottom: 0.5em;
     }
-    to {
-
+    100% {
+        bottom: 3.5em;
     }
 }
-.three-dots .slideout {
-    -moz-transform: rotate(30deg);
-    transform: rotate(30deg);
+.button-one.slideout {
+    animation: button-one-slide-up 0.6s ease;
+    animation-fill-mode: both;
 }
-.floating-button-container {
-    display: flex;
-    gap: 0.4em;
+.button-two.slideout {
+    animation: button-two-slide-up 0.55s ease;
+    animation-fill-mode: both;
+}
+.three-dots {
+    transition: all 0.5s ease;
+    transform: rotate(0deg);
+}
+.three-dots.slideout {
+    transform: rotate(90deg);
+    transition-duration: 0.5s;
+    transition-timing-function: ease;
+}
+.floating-button {
     position: absolute;
     bottom: 0.5em;
     right: 0.5em;
-    flex-direction: column;
-}
-.floating-button {
-  color: white;
-  text-align: center;
-  font-size: 40px;
-  line-height: 45px;
-  width: 50px;
-  height: 50px;
-  /* background-color: #3B3561; */
-  border-radius: 50%;
-  box-shadow: 0px 2px 4px 2px #F7EBE8;
+    color: white;
+    text-align: center;
+    font-size: 40px;
+    line-height: 45px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    box-shadow: 0px 2px 4px 2px #F7EBE8;
 }
 .floating-button:hover {
     cursor: pointer;
+}
+.link, .link:hover {
+    text-decoration: none;
+    color: white;
 }
 </style>
